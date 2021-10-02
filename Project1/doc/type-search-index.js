666f64ce096814e058f6c1417bc4f619b7c60abb
brianwu201 <90476352+brianwu201@users.noreply.github.com>
2021-10-02 15:06:40 -0400

    Add files via upload

---
+++ Project1/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"project1","l":"Album"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"project1","l":"Collection"},{"p":"project1","l":"CollectionManager"},{"p":"project1","l":"Date"},{"p":"project1","l":"Genre"},{"p":"project1","l":"RunProject1"}];updateSearchResults();
